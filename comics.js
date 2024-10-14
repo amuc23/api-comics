d1d269e8d8050e8f37e5c64ea056351943741a8d
Alonso <amuc23@gmail.com>
2024-10-13 23:08:02 -0300

    cambios de imagenes 2

--- comics.js
+++ comics.js
@@ -4,42 +4,42 @@ const comics = [
     "titulo": "Spider-Man",
     "autor": "Stan Lee",
     "anio": 1962,
-    "imagen": "https://fdsl.cl/imgs-prog/comics/hombrequearana.jpg"
+    "imagen": "https://cdnx.jumpseller.com/shazam-online/image/34059202/sgrsp001_0.jpg?1681238240"
   },
   {
     "id": 2,
     "titulo": "Iron Man",
     "autor": "Stan Lee",
     "anio": 1963,
-    "imagen": "https://fdsl.cl/imgs-prog/comics/ironman.jpg"
+    "imagen": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgynFWhQoNfxUKfMN9zIcpjaVeX3QqyUNpk7tD5vJelALORb4tm8VpsoPIgFkVB9ljzaX_2LfkfEvs8l1kz9ffEr8cSFuypLyD2KMKhyvlLwIJBkg2qy9mTstijT6Wd-mpiECXHFJRQr5qLZzgFQq3LwpeJSU2TtibxIpQpenIub3pKlNRp4gaSeI4/s4182/INVIM2022001_BAGLEY_SECOND-PRINT-COVER.jpg"
   },
   {
     "id": 3,
     "titulo": "Thor",
     "autor": "Stan Lee",
     "anio": 1962,
-    "imagen": "https://fdsl.cl/imgs-prog/comics/thortilla.jpg"
+    "imagen": "https://m.media-amazon.com/images/I/714QCPd0+pL._AC_UF1000,1000_QL80_.jpg"
   },
   {
     "id": 4,
     "titulo": "Captain America",
     "autor": "Joe Simon y Jack Kirby",
     "anio": 1941,
-    "imagen": "https://fdsl.cl/imgs-prog/comics/capitanamerica.jpg"
+    "imagen": "https://cdn.marvel.com/u/prod/marvel/i/mg/6/50/5aa17558af435/portrait_uncanny.jpg"
   },
   {
     "id": 5,
     "titulo": "Doctor Strange",
     "autor": "Steve Ditko",
     "anio": 1963,
-    "imagen": "https://fdsl.cl/imgs-prog/comics/doctorstrange.jpg"
+    "imagen": "https://static.wikia.nocookie.net/marveldatabase/images/6/63/Dr._Strange_Vol_1_1.jpg/revision/latest?cb=20191204233552"
   },
   {
     "id": 6,
     "titulo": "Black Panther",
     "autor": "Stan Lee y Jack Kirby",
     "anio": 1966,
-    "imagen": "https://fdsl.cl/imgs-prog/comics/chocopantera.jpg"
+    "imagen": "https://www.latercera.com/resizer/v2/Z3LQURB4SBG6XHY4IC6JLQV564.jpg?quality=80&smart=true&auth=bd466776232b424637f3a2c85c0e2c25e79debf9aaf4f20b2be020b71bde7726&width=790&height=1197"
   }
 ];
 
