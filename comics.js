c517a8cc00bfdac6d3c62fbaa26224438b919fcb
Alonso <amuc23@gmail.com>
2024-10-13 22:49:22 -0300

    cambios de foto

--- comics.js
+++ comics.js
@@ -4,42 +4,42 @@ const comics = [
     "titulo": "Spider-Man",
     "autor": "Stan Lee",
     "anio": 1962,
-    "imagen": "https://i.blogs.es/4d5a9c/spiderman-4/1366_2000.jpeg"
+    "imagen": "https://fdsl.cl/imgs-prog/comics/hombrequearana.jpg"
   },
   {
     "id": 2,
     "titulo": "Iron Man",
     "autor": "Stan Lee",
     "anio": 1963,
-    "imagen": "https://wallpapers.com/images/hd/iron-man-comics-1313-x-2049-wallpaper-cckqwr06kqjwhljf.jpg"
+    "imagen": "https://fdsl.cl/imgs-prog/comics/ironman.jpg"
   },
   {
     "id": 3,
     "titulo": "Thor",
     "autor": "Stan Lee",
     "anio": 1962,
-    "imagen": "https://www.shopknuckleheads.com/cdn/shop/products/29920MV_1200x1680.jpg?v=1648744917"
+    "imagen": "https://fdsl.cl/imgs-prog/comics/thortilla.jpg"
   },
   {
     "id": 4,
     "titulo": "Captain America",
     "autor": "Joe Simon y Jack Kirby",
     "anio": 1941,
-    "imagen": "https://cdnx.jumpseller.com/shazam-online/image/23044149/MustHave_LaMuertedelCapitanAmerica.jpeg?1648495282"
+    "imagen": "https://fdsl.cl/imgs-prog/comics/capitanamerica.jpg"
   },
   {
     "id": 5,
     "titulo": "Doctor Strange",
     "autor": "Steve Ditko",
     "anio": 1963,
-    "imagen": "https://i.pinimg.com/originals/95/c4/84/95c484db95ba8ad40580d6660d7b0bbd.jpg"
+    "imagen": "https://fdsl.cl/imgs-prog/comics/doctorstrange.jpg"
   },
   {
     "id": 6,
     "titulo": "Black Panther",
     "autor": "Stan Lee y Jack Kirby",
     "anio": 1966,
-    "imagen": "https://i.pinimg.com/originals/6e/1e/b4/6e1eb4994322893b5457f693a5e61550.jpg"
+    "imagen": "https://fdsl.cl/imgs-prog/comics/chocopantera.jpg"
   }
 ];
 
